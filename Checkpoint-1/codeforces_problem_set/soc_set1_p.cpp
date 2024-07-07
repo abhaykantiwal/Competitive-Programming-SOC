e66a636d0de4ecba5c6be8f819a743da694af6ea
abhaykantiwal <146532691+abhaykantiwal@users.noreply.github.com>
2024-07-07 21:07:33 +0530

    Update soc_set1_p.cpp

--- Checkpoint-1/codeforces_problem_set/soc_set1_p.cpp
+++ Checkpoint-1/codeforces_problem_set/soc_set1_p.cpp
@@ -1,1 +1,12 @@
-
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int a1,a2,a3;
+    cin>>a1>>a2>>a3;
+    int a = sqrt((a1*a2)/a3);
+    int b = sqrt((a1*a3)/a2);
+    int c = sqrt((a3*a2)/a1);
+    int ans = 4*(a+b+c);
+    cout<<ans<<"\n";
+    return 0;
+}
